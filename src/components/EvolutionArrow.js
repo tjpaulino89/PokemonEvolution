69cddf6e9097b5be32d7f316dadb42371ba5966c
Thomas Paulino <tjpaulino89@gmail.com>
2023-03-13 16:50:03 -0400

    edit evolve arrow

--- src/components/EvolutionArrow.js
+++ src/components/EvolutionArrow.js
@@ -4,8 +4,7 @@ const EvolutionArrow = () => {
     return (
         <div className='tc bg-light-yellow dib br3 pa1 bw2 shadow-5'>
             <h3 className='ma1'>Evolves</h3>
-            <h3 className='ma1'>to</h3>
-            <h3 className='ma1'>{'->'}</h3>
+            <h3 className='ma1'>to:</h3>
         </div>
     );
 }
